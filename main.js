b3575eb957b30124c4cf0821bd2f350c04f5bf61
kalebeofc <kalebeofc@gmail.com>
2025-06-04 22:49:30 +0000

    showItemsList criada

--- main.js
+++ main.js
@@ -1,14 +1,39 @@
 const items = []
 
-function addItem (){
-const itemName = document.querySelector('#item').value
+function addItem() {
+    const itemName = document.querySelector('#item').value
 
-const item ={
-    name: itemName,
-    checked: false,
+    const item = {
+        name: itemName,
+        checked: false,
+    }
+
+    items.push(item)
+
+    document.querySelector('#item').value = ""
+
+    showItemsList()
 }
 
-items.push(item)
+function showItemsList() {
+    const selectionlist = document.querySelector('.list')
+    selectionlist.textContent=""
 
-document.querySelector('#item').value = ""
+    items.map((item, index) => {
+        selectionlist.innerHTML += `
+         <div class="item">
+                <div>
+                    <input type="checkbox" name="list" id="item-${index}">
+
+                    <div class="custom-checkbox">
+                        <img src="./assets/checked.svg" alt="checked">
+                    </div>
+                    <label for="item-${index}">${item.name}</label>
+                </div>
+                <button>
+                    <img src="./assets/trash-icon.svg" alt="trash-icon">
+                </button>
+            </div>
+         `
+    })
 }
